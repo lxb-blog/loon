2249d7b12e66d4cb6c8f70ac58c3249f12fab51d
lxb-blog <97530887+lxb-blog@users.noreply.github.com>
2025-03-15 15:30:59 +0800

    Update 233.js

--- 233.js
+++ 233.js
@@ -1,17 +1,18 @@
-// Surge 脚本 - 修改 isFreeListen 为 1
 let body = $response.body;
 let obj = JSON.parse(body);
 
 // 遍历数据结构，修改所有 isFreeListen 的值
 function modifyIsFreeListen(data) {
     if (Array.isArray(data)) {
-        data.forEach(item => modifyIsFreeListen(item));
+        data.forEach(item => modifyIsFreeListen(item)); // 如果是数组，递归遍历每个项
     } else if (typeof data === "object") {
         for (let key in data) {
             if (key === "isFreeListen") {
                 data[key] = 1; // 强制修改为 1
-            } else {
-                modifyIsFreeListen(data[key]); // 递归修改子对象
+            } else if (Array.isArray(data[key])) {
+                modifyIsFreeListen(data[key]); // 如果是数组，递归遍历数组中的元素
+            } else if (typeof data[key] === "object") {
+                modifyIsFreeListen(data[key]); // 如果是对象，递归遍历对象中的字段
             }
         }
     }
