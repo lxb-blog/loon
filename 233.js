06c0ecec940d50cf9f31a95171b5fc938285fa54
lxb-blog <97530887+lxb-blog@users.noreply.github.com>
2025-03-15 15:34:59 +0800

    Update 233.js

--- 233.js
+++ 233.js
@@ -1,36 +1,6 @@
 let body = $response.body;
-let obj = JSON.parse(body);
 
-// 遍历数据结构，修改所有 isFreeListen 的值
-function modifyIsFreeListen(data) {
-    if (Array.isArray(data)) {
-        data.forEach(item => modifyIsFreeListen(item)); // 如果是数组，递归遍历每个项
-    } else if (typeof data === "object") {
-        for (let key in data) {
-            if (key === "isFreeListen") {
-                data[key] = 1; // 强制修改为 1
-            } else if (key === "courseChapterRspList") {
-                // 针对章节列表，遍历每个章节
-                data[key].forEach(chapter => {
-                    if (chapter.chapterDetailRspList) {
-                        chapter.chapterDetailRspList.forEach(detail => {
-                            if (detail.isFreeListen !== undefined) {
-                                detail.isFreeListen = 1; // 修改章节详情中的 isFreeListen 字段
-                            }
-                        });
-                    }
-                });
-            } else if (Array.isArray(data[key])) {
-                modifyIsFreeListen(data[key]); // 如果是数组，递归遍历数组中的元素
-            } else if (typeof data[key] === "object") {
-                modifyIsFreeListen(data[key]); // 如果是对象，递归遍历对象中的字段
-            }
-        }
-    }
-}
+// 使用正则表达式匹配所有的 isFreeListen 字段并修改它们的值
+body = body.replace(/("isFreeListen"\s*:\s*)\d+/g, '$1 1');  // 匹配所有的 isFreeListen 字段并把值改为 1
 
-// 修改响应数据
-modifyIsFreeListen(obj);
-
-// 返回修改后的数据
-$done({ body: JSON.stringify(obj) });
+$done({ body });
