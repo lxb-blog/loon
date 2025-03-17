713f78a7e4e2e187839764bba5fd10cccaff742f
lxb-blog <97530887+lxb-blog@users.noreply.github.com>
2025-03-17 08:38:24 +0800

    Update 233.js

--- 233.js
+++ 233.js
@@ -3,7 +3,7 @@ const targetURL = 'https://japi.233.com/ess-study-api/learn/do/list-chapter-by-v
 if ($response.status === 200 && $request.url.indexOf(targetURL) !== -1) {
     try {
         let body = JSON.parse($response.body);
-        deepModify(body, 'isFreeListen', 1);
+        deepModify(body, 'isFreeListen', 1); // 修改此处参数为 1
         $done({ body: JSON.stringify(body) });
     } catch (error) {
         console.log(`Error processing response: ${error}`);
@@ -13,14 +13,13 @@ if ($response.status === 200 && $request.url.indexOf(targetURL) !== -1) {
     $done({});
 }
 
-// 递归遍历对象修改指定属性
 function deepModify(obj, targetKey, newValue) {
     if (obj instanceof Array) {
         obj.forEach(item => deepModify(item, targetKey, newValue));
     } else if (obj instanceof Object) {
         for (const key in obj) {
             if (key === targetKey) {
-                obj[key] = newValue;
+                obj[key] = newValue; // 值已改为 1
             } else {
                 deepModify(obj[key], targetKey, newValue);
             }
